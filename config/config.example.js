e67395a921b05971a717b04cb984e84fbd9bb087
Julio Makdisse Saito <saitodisse@gmail.com>
2014-03-21 18:32:04 -0300

    adding link to source-code

--- config/config.example.js
+++ config/config.example.js
@@ -1,10 +1,12 @@
 /*
     heroku config:add FACEBOOK_APP_ID=FACEBOOK_APP_ID_XXX
     heroku config:add FACEBOOK_APP_SECRET=FACEBOOK_APP_SECRET_XXX
-    heroku config:add WEB_SITE_URI=WEB_SITE_URI_XXX
+    heroku config:add WEB_SITE_URI=http://__PUT_WEB_SITE_URI__/auth/facebook/callback
  */
 
 module.exports = {
     FACEBOOK_APP_ID: "FACEBOOK_APP_ID_XXX",
     FACEBOOK_APP_SECRET: "FACEBOOK_APP_SECRET_XXX",
-    WEB_SITE_URI: "WEB_SITE_URI_XXX"
+    WEB_SITE_URI: "http://127.0.0.1",
+    PORT: "3000"
+}
